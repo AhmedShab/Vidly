5ab863d4126b003a4c8214620fffee586b1b85dd
AhmedShab <ahmed.vuw@gmail.com>
2016-10-02 16:33:58 +1300

    added list of customer viewModel class. Working on rendering the customers now

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,8 +23,20 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie { Name = "Final Fantasy" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
 
         }
 
