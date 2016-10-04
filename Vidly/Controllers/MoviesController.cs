4954585f48f797e45a202fcbd418a85d38416858
AhmedShab <ahmed.vuw@gmail.com>
2016-10-05 11:19:29 +1300

    added new, edit and save methods in the movie controller. At this stage, you can edit, add and update new movie. Working on the validation error

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -54,7 +54,17 @@ namespace Vidly.Controllers
 
         public ActionResult Edit(int id)
         {
-            return View("MovieForm");
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null) return HttpNotFound();
+
+            var newModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", newModel);
         }
 
         public ActionResult Details(int id)
@@ -77,6 +87,25 @@ namespace Vidly.Controllers
                 Genres = genres
             };
             return View("MovieForm", newModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0) _context.Movies.Add(movie);
+
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.Genre = movie.Genre;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
