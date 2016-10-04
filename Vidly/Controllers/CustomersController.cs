9f93e7d5c0cdcfb4a87a10452d4b7e1c3f1125b6
AhmedShab <ahmed.vuw@gmail.com>
2016-10-04 15:07:08 +1300

    renamed 'Create' action to 'Save'

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -50,9 +50,19 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0 ) _context.Customers.Add(customer);
+
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.IsSubscripedToNewsLetters = customer.IsSubscripedToNewsLetters;
+            }
+
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
